e3f7ebed89a9c1a7aed53a738d5b8d019f736b59
Thomas Trogdon <tom.trogdon@gmail.com>
2014-10-18 15:14:32 -0400

    Added OPZeros functionality

--- RiemannHilbert/RandomMatrices/init.m
+++ RiemannHilbert/RandomMatrices/init.m
@@ -1,5 +1,8 @@
+(* ::Package:: *)
+
 (* Mathematica package *)
 Get[ "RiemannHilbert`RandomMatrices`EquilibriumMeasure`"]
 Get[ "RiemannHilbert`RandomMatrices`FreeProbability`"]
 Get[ "RiemannHilbert`RandomMatrices`MatrixDistributions`"]
 Get[ "RiemannHilbert`RandomMatrices`OrthogonalPolynomials`"]
+Get[ "RiemannHilbert`RandomMatrices`OPZeros`"]
